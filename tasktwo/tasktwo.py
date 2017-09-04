9f10c9a0b0942c96129b4d3a0a1d3525231c9b16
Ali Sajid Imami <ali.sajid.imami@gmail.com>
2017-09-04 23:16:47 +0500

    Task two stub

---
+++ tasktwo/tasktwo.py
@@ -0,0 +1,9 @@
+"""
+This file contains the code required to apply for certification as a Nationbuilder Developer.
+
+I am required to do two tasks.
+For Task Two I've chosen the Surveys + Contact section. I will be attempting all three tasks in it:
+
+1. Create a Survey
+2. Log a contact when someone answers a survey question.
+"""
